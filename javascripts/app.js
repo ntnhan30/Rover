51fd3100e82331de3bf28fcabb487642fd4e9497
ntnhan30 <nhannguyen@Trongs-MacBook-Air.local>
2018-07-17 15:55:03 +0200

    final

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,31 +1,28 @@
 // Rover Object Goes Here
-// ======================
-
-// ======================
 
 var rover1 = {
   direction :"E",
-  row:2,
-  col:5,
+  row:4,
+  col:4,
   travelLog:[]
 } 
 
 var rover2 = {
   direction :"E",
   row:1,
-  col:9,
+  col:3,
   travelLog:[]
 } 
 
-
+//true: free; false: obstacle or other rover. 
 var grid = [
   [true, true, true, true, true,true, true, true, true ,true],
-  [true, true, true, true, true,true, true, true, true ,true],
-  [true, true, true, true, true,true, true, true, true ,true],
-  [true, true, true, false, true,true, true, true, true ,true],
+  [true, true, true, true,false,true, true, true, true ,true],
   [true, true, false, true, true,true, true, true, true ,true],
-  [true, false, true, true, true,true, true, true, true ,true],
   [true, true, true, false, true,true, true, true, true ,true],
+  [true, true, false, true, false,true, true, false, true ,true],
+  [true, false, true, true, true,true, true, true, true ,true],
+  [true, true, true, false, true,false, true, true, true ,true],
   [true, true, true, true, true,true, true, true, true ,true],
   [true, true, true, true, true,true, true, true, true ,true],
   [true, true, true, true, true,true, true, true, true ,false],
@@ -184,9 +181,10 @@ function moveBackward(rover){
 }
 
 function commands (lists, rover) {
-  var roverCurrentPosition= rover.travelLog[rover.travelLog.length -1];
-  var roverPreviousPosition= rover.travelLog[rover.travelLog.length-2];
+  rover.travelLog.push([rover.row,rover.col]);
+  
     for (i=0;i< lists.length; i++) {
+     
      if (lists[i]=== "l" || lists[i]==="r"|| lists[i]=== "b"|| lists[i]=== "f") {
       switch (lists[i]) {
        case "l":
@@ -198,24 +196,27 @@ function commands (lists, rover) {
         turnRight(rover)
        break;
       case "f":
-         console.log(lists[i]);
-         moveForward(rover);
-        if (roverCurrentPosition !== undefined) {
-         console.log ("currenr position: "+ roverCurrentPosition);
-         grid[roverCurrentPosition[0]][roverCurrentPosition[1]]=false ; }
-         // console.log("Value of current position : "+ grid[roverCurrentPosition[0]][roverCurrentPosition[1]]);
+        console.log(lists[i]);
+        moveForward(rover);
+        var roverPreviousPosition= rover.travelLog[rover.travelLog.length-2];
+        var roverCurrentPosition= rover.travelLog[rover.travelLog.length -1];
+        console.log ( "Rover current position: "+roverCurrentPosition);
+        grid[roverCurrentPosition[0]][roverCurrentPosition[1]]=false ; 
+        // console.log("Value of current position : "+ grid[roverCurrentPosition[0]][roverCurrentPosition[1]]);
         if (roverPreviousPosition !== undefined)
-         //  { console.log ("previous position: "+ roverPreviousPosition);
+          { 
+            // console.log ("previous position: "+ roverPreviousPosition);
          grid[roverPreviousPosition[0]][roverPreviousPosition[1]]=true ; 
-        // console.log("Value of previous position : "+ grid[roverPreviousPosition[0]][roverPreviousPosition[1]]);}
-
+        // console.log("Value of previous position : "+ grid[roverPreviousPosition[0]][roverPreviousPosition[1]]);
+      }
       break;
       case "b":
         console.log(lists[i]);
         moveBackward(rover);
-        console.log (rover.travelLog[rover.travelLog.length -1]);
-        if (roverCurrentPosition !== undefined) {
-         grid[roverCurrentPosition[0]][roverCurrentPosition[1]]=false ; }
+        var roverPreviousPosition= rover.travelLog[rover.travelLog.length-2];
+        var roverCurrentPosition= rover.travelLog[rover.travelLog.length -1];
+        console.log ("Rover current position is "+roverCurrentPosition);
+         grid[roverCurrentPosition[0]][roverCurrentPosition[1]]=false ; 
         if (roverPreviousPosition !== undefined)
          grid[roverPreviousPosition[0]][roverPreviousPosition[1]]=true ; 
       break;
@@ -232,9 +233,8 @@ function commands (lists, rover) {
 
 // console.log(grid[rover1.row][rover1.col])
 
-    commands ("frfccbflftzlbf",rover1);
-    // commands ("frfccbflftzlbf",rover1);
-
+    commands ("frfccbflftzlbf",rover2);
+    
  
 
 
